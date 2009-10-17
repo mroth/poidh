fd5c98f7c49d00232c489789082bd98af4ccb12f
Matthew Rothenberg <mrothenberg@gmail.com>
2009-10-16 23:44:38 -0700

    better tweet link formatting

--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -1,3 +1,12 @@
 class Tweet < ActiveRecord::Base
   validates_uniqueness_of :observer_msg_id
+  
+  def observer_msg_url
+    return "http://twitter.com/" + observer_screen_name + "/status/" + observer_msg_id.to_s
+  end
+  
+  def culprit_msg_url
+    return "http://twitter.com/" + culprit_screen_name + "/status/" + culprit_msg_id.to_s
+  end
+  
 end
