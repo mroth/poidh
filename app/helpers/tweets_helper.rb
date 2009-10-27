900da90d14ace63ef0f316807267f639a06c721e
Matthew Rothenberg <mrothenberg@gmail.com>
2009-10-26 20:26:53 -0700

    moved avatars to twivatar.org

--- app/helpers/tweets_helper.rb
+++ app/helpers/tweets_helper.rb
@@ -7,4 +7,13 @@ module TweetsHelper
   def twitter_member_url(screenname)
     "http://twitter.com/" + screenname
   end
+  
+  def twitter_avatar_img(screenname)
+    image_tag("http://twivatar.org/" + screenname, :size => '48x48')
+  end
+  
+  def twitter_avatar_mini_img(screenname)
+    image_tag("http://twivatar.org/" + screenname + "/mini", :size => "24x24")
+  end
+  
 end
