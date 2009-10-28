d5ff2f0e69c357d5be6a77695329b59ea9d1826e
Matthew Rothenberg <mrothenberg@gmail.com>
2009-10-27 22:54:55 -0700

    slightly slower polling rate

--- config/schedule.rb
+++ config/schedule.rb
@@ -3,7 +3,7 @@
 # It's helpful, but not entirely necessary to understand cron before proceeding.
 # http://en.wikipedia.org/wiki/Cron
 
-every 10.minutes do
+every 15.minutes do
   rake "twitter:spider"
 end
 
