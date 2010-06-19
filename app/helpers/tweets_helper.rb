7afe7689d133e1e3758f4cb5ea71c9498a3683cc
Matthew Rothenberg <mrothenberg@gmail.com>
2010-06-19 12:59:27 -0700

    replaced twivatar with tweetimage service

--- app/helpers/tweets_helper.rb
+++ app/helpers/tweets_helper.rb
@@ -9,11 +9,13 @@ module TweetsHelper
   end
   
   def twitter_avatar_img(screenname)
-    image_tag("http://twivatar.org/" + screenname, :size => '48x48', :alt => screenname)
+    #image_tag("http://twivatar.org/" + screenname, :size => '48x48', :alt => screenname)
+    image_tag("http://img.tweetimag.es/i/" + screenname + "_n", :size => "48x48", :alt => screenname)
   end
   
   def twitter_avatar_mini_img(screenname)
-    image_tag("http://twivatar.org/" + screenname + "/mini", :size => "24x24", :alt => screenname)
+    #image_tag("http://twivatar.org/" + screenname + "/mini", :size => "24x24", :alt => screenname)
+    image_tag("http://img.tweetimag.es/i/" + screenname + "_m", :size => "24x24", :alt => screenname)
   end
   
 end
